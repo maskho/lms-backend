a28b3899df28792276999e7f3e2b9d686264f788
maskho <maskhobar@gmail.com>
2023-03-31 20:40:39 +0700

    create db, schema

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 import express from "express";
 import cors from "cors";
+import { readdirSync } from "fs";
+import mongoose from "mongoose";
 const morgan = require("morgan");
 require("dotenv").config();
 
 const app = express();
 
+mongoose
+  .connect(process.env.DATABASE, {})
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB Error: ", err));
+
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.get("/", (req, res) => {
-  res.send("test hit");
-});
+readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
 const port = process.env.PORT || 8000;
 
